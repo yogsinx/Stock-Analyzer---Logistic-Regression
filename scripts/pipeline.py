d4bebfefb4d0cf39d06ef1ddf797d39e79fb8fbe
YogSingh <sin.yog1310@gmail.com>
2025-04-03 20:03:20 +0530

    Adding scripts as default module import

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,7 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from scripts.database import create_stock_table
 from scripts.model_train import train_model
 from scripts.model_predict import predict_stock
-import os
 
 # Define directories for raw data and model saving
 raw_data_dir = "../data/raw"
